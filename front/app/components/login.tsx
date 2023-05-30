868f58ceb8a07b96fa794878aa22a3ebf589e0e2
fallen8 <sorksoek2@gmail.com>
2023-05-30 17:11:44 +0900

    WIP: login responsive login modal

--- front/app/components/login.tsx
+++ front/app/components/login.tsx
@@ -13,55 +13,56 @@ export default function Login() {
 	const [password, setPassword] = useState("");
 
 	return (
-		<div className={`min-w-[300px] fixed p-6 ${state.isLoginOpen ? "top-0 scale-100" : "top-full scale-50"} w-full h-full transition-all duration-300 bg-white`}>
-			<div className="w-full flex justify-end">
-				<button onClick={() => dispatch('closeLogin')}>
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-			</div>
-
-			<div className="font-noto w-full mt-10 grid gap-y-6">
-				<h2 className="text-xl font-bold">{signUp ? "회원가입" : "로그인"}</h2>
-				<h4 className="font-bold text-black text-opacity-40">아이디로 {signUp ? "회원가입" : "로그인"}</h4>
-				<div className="w-full flex">
-					<form className="w-full" onSubmit={async (e: FormEvent) => {
-							const res = await	Submit(e, id, password, signUp);
-							if (res && signUp) {
-								setSignUp(false);
-								setId("");
-								setPassword("");
-								return;
-							}
-
-							if (res && !signUp) {
-								dispatch({type: "closeLogin"});
-								dispatch({type: "login"});
-								setId("");
-								setPassword("");
-							}
-						}
-					}>
-						<div className="divide-y border">
-							<input value={id} onChange={(e) => setId(e.target.value)} id="id" className="w-full p-3 focus-within:outline-red-200" type="text" placeholder="아이디를 입력하세요." />
-							<input value={password} onChange={(e) => setPassword(e.target.value)} id="password" className="w-full p-3 focus-within:outline-red-200" type="password" placeholder={`${signUp ? "영문 숫자 특수기호 조합 8~15 자리." : "비밀번호를 입력하세요."}`} />
-						</div>
-						<button className="w-full p-3 whitespace-nowrap min-w-[100px] flex justify-center text-center bg-red-400 text-white hover:bg-red-300" type="submit">
-							{signUp ? "회원가입" : "로그인"}
-						</button>
-					</form>
-				</div>
-			</div>
-
-			<div className="absolute bottom-5 right-5">
-				{ signUp ? "이미 회원이신가요 ?" :"아직 회원이 아니신가요 ?"}
-				<span>
-					&nbsp;&nbsp;
-					<button onClick={() => setSignUp(!signUp)} className=" text-red-400">
-						{signUp ? "로그인" : "회원가입"}
+		<div className={`fixed w-full h-full min-w-[300px] bg-white ${state.isLoginOpen ? "top-0" : "top-full"}  phone:flex phone:justify-center phone:items-center phone:bg-opacity-70 phone:bg-none`}>
+			<div className={`relative w-full h-full p-6 bg-white transition-transform duration-500 phone:w-[390px] phone:h-[500px] phone:shadow-2xl`}>
+				<div className="w-full flex justify-end ">
+					<button onClick={() => dispatch('closeLogin')}>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
 					</button>
-				</span>
+				</div>
+				<div className="font-noto w-full mt-10 grid gap-y-6">
+					<h2 className="text-xl font-bold">{signUp ? "회원가입" : "로그인"}</h2>
+					<h4 className="font-bold text-black text-opacity-40">아이디로 {signUp ? "회원가입" : "로그인"}</h4>
+					<div className="w-full flex">
+						<form className="w-full" onSubmit={async (e: FormEvent) => {
+								const res = await	Submit(e, id, password, signUp);
+								if (res && signUp) {
+									setSignUp(false);
+									setId("");
+									setPassword("");
+									document.getElementById("id")?.focus();
+									return;
+								}
+
+								if (res && !signUp) {
+									dispatch({type: "closeLogin"});
+									dispatch({type: "login"});
+									setId("");
+									setPassword("");
+								}
+							}
+						}>
+							<div className="divide-y border">
+								<input value={id} onChange={(e) => setId(e.target.value)} id="id" className="w-full p-3 focus-within:outline-red-200" type="text" placeholder="아이디를 입력하세요." />
+								<input value={password} onChange={(e) => setPassword(e.target.value)} id="password" className="w-full p-3 focus-within:outline-red-200" type="password" placeholder={`${signUp ? "영문 숫자 특수기호 조합 8~15 자리." : "비밀번호를 입력하세요."}`} />
+							</div>
+							<button className="w-full p-3 whitespace-nowrap min-w-[100px] flex justify-center text-center bg-red-400 text-white hover:bg-red-300" type="submit">
+								{signUp ? "회원가입" : "로그인"}
+							</button>
+						</form>
+					</div>
+				</div>
+				<div className="absolute bottom-5 right-5">
+					{ signUp ? "이미 회원이신가요 ?" :"아직 회원이 아니신가요 ?"}
+					<span>
+						&nbsp;&nbsp;
+						<button onClick={() => setSignUp(!signUp)} className=" text-red-400">
+							{signUp ? "로그인" : "회원가입"}
+						</button>
+					</span>
+				</div>
 			</div>
 		</div>
 	)
@@ -109,7 +110,7 @@ const SignIn = async (id: string, password: string): Promise<boolean> => {
 }
 
 const SignUp = async (id: string, password: string): Promise<boolean> => {
-	const regex = new RegExp(process.env.NEXT_PUBLIC_PASSWORD_REGEX ? process.env.NEXT_PUBLIC_PASSWORD_REGEX.toString() : /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/);
+	const regex = new RegExp(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/);
 	if (!regex.test(password)) {
 		ToastWraper("error", "비밀번호 규칙을 확인해주세요.");
 		return false;
