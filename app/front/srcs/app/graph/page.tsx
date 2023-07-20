37ef98cf1fdfcb385b67ea6d66779e271aa2393a
fallen8 <sorksoek2@gmail.com>
2023-07-20 22:14:49 +0900

    마우스 드래그 이벤트까지 완료

--- app/front/srcs/app/graph/page.tsx
+++ app/front/srcs/app/graph/page.tsx
@@ -56,16 +56,78 @@ const data: {
 }
 
 export default function Graph() {
-	useEffect(() => {
+	const WIDTH = window.innerWidth;
+	const HEIGHT = window.innerHeight;
 
+	useEffect(() => {
+		const svg = d3.select("svg");
+		svg.attr("width", WIDTH).attr("height", HEIGHT);
+
+		const links = svg.append("g")
+			.selectAll("line")
+			.data(data.links)
+			.enter()
+			.append("line")
+			.attr("stroke-width", 3)
+			.style("stroke", "pink");
+
+		const nodes = svg.append("g")
+			.selectAll("circle")
+			.data(data.nodes)
+			.enter()
+			.append("circle")
+			.attr("r", "10")
+			.style("fill", "orange");
+
+		function ticked() {
+			links
+				.attr("x1", (d) => d.source.x)
+				.attr("y1", (d) => d.source.y)
+				.attr("x2", (d) => d.target.x)
+				.attr("y2", (d) => d.target.y)
+
+			nodes
+				.attr("cx", (d) => d.x)
+				.attr("cy", (d) => d.y)
+		}
+
+		const simulation = d3.forceSimulation(data.nodes)
+			.force("link", d3.forceLink(data.links).id(d => d.id || 1))
+			.force("charge", d3.forceManyBody().strength(-50))
+			.force("center", d3.forceCenter(WIDTH / 2, HEIGHT / 2))
+			.on("tick", ticked);
+
+		function dragStarted(d) {
+			simulation.alphaTarget(0.3).restart();
+			d.subject.fx = d.subject.x;
+			d.subject.fy = d.subject.y;
+		}
+
+		function dragged(d) {
+			d.subject.fx = d.x;
+			d.subject.fy = d.y;
+		}
+
+		function dragEnded(d) {
+			simulation.alphaTarget(0);
+			d.subject.fx = null;
+			d.subject.fy = null;
+		}
+
+		const drag = d3.drag()
+			.on("start", dragStarted)
+			.on("drag", dragged)
+			.on("end", dragEnded);
 		
+		nodes.call(drag)
 	}, [])
 
 
 	return (
 		<div className="relative flex w-full">
-			<div className="flex gap-10 p-32" id="box">
-			</div>
+			<svg>
+
+			</svg>
 		</div>
 	)
 }
